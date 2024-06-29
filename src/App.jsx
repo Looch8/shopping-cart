d8bce3263e220dceee41faacf67757e6a62d7278
Luke Wheldale <lukewheldale@gmail.com>
2024-06-29 14:31:03 +0930

    bootstrap, react router, navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
-	return <></>;
+	return (
+		<>
+			<NavBar />
+			<h1>Shopping Cart</h1>
+		</>
+	);
 }
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import "../styles/NavBar.css";
+
+const NavBar = () => {
+	return (
+		<ul className="list-group">
+			<li className="list-group-item">Home</li>
+			<li className="list-group-item">Shop</li>
+		</ul>
+	);
+};
+
+export default NavBar;
